ef5187634b83e14262d1e476d005ef08899098a4
gamethomes38 <gamethomes38@gmail.com>
2023-04-08 20:57:46 +0800

    updated mysql server setting

--- backend/mysql/init_database.sql
+++ backend/mysql/init_database.sql
@@ -1,36 +1,30 @@
 CREATE DATABASE test;
 USE test;
 
-CREATE TABLE customers(
-    CustomerUUID VARBINARY(16) DEFAULT (RANDOM_BYTES(16)) NOT NULL PRIMARY KEY,
-    CustomerName VARCHAR(200) NOT NULL,
-    Email VARCHAR(200)NOT NULL,
-    CustomerPassword varchar(255) NOT NULL,
-    Age TINYINT UNSIGNED,
-    CustomerAddress TEXT DEFAULT NULL,
-)
+CREATE TABLE `customers`(
+    `uuid` VARBINARY(16) DEFAULT (RANDOM_BYTES(16)) NOT NULL PRIMARY KEY,
+    `name` VARCHAR(200) NOT NULL,
+    `email` VARCHAR(200)NOT NULL,
+    `password` varchar(255) default null,
+    `age` TINYINT UNSIGNED,
+    `address` TEXT DEFAULT NULL,
+    `pan` VARCHAR(20),
+    `cvv` VARCHAR(3)
+);
 
-CREATE TABLE products(
-    ProductID int NOT NULL PRIMARY KEY,
-    ProductName VARCHAR(200) NOT NULL,
-    Price FLOAT(6,2)NOT NULL,
-    TotalQuantity INT UNSIGNED NOT NULL,
-    AvailableQuantity INT UNSIGNED NOT NULL, 
-    HoldedQuantity INT UNSIGNED NOT NULL,
-)
+CREATE TABLE `products`(
+    `id` INT NOT NULL PRIMARY KEY,
+    `name` VARCHAR(200) NOT NULL,
+    `price` FLOAT(6,2)NOT NULL,
+    `total_quantity` INT UNSIGNED NOT NULL,
+    `available_quantity` INT UNSIGNED NOT NULL, 
+    `holded_quantity` INT UNSIGNED NOT NULL
+);
 
-CREATE TABLE administrators(
-    AdminUUID VARBINARY(16) DEFAULT (RANDOM_BYTES(16)) NOT NULL PRIMARY KEY,
-    AdminName VARCHAR(200) NOT NULL,
-    Email VARCHAR(200)NOT NULL,
-    AdminPassword varchar(255) NOT NULL,
-)
+CREATE TABLE `administrators`(
+    `uuid` VARBINARY(16) DEFAULT (RANDOM_BYTES(16)) NOT NULL PRIMARY KEY,
+    `name` VARCHAR(200) NOT NULL,
+    `email` VARCHAR(200)NOT NULL,
+    `assword` varchar(255) NOT NULL
+);
 
-
-INSERT INTO customers (`name`,`email`,`address`,`pan`,`cvv`)
-VALUES
-  ("Aretha Ward","ut.sagittis@outlook.couk","5658 Nec, Rd.","4913 3175 4987 6633","176"),
-  ("Candace Hansen","tempor.erat@google.couk","P.O. Box 921, 3235 Nullam Rd.","453958 194672 7814","119"),
-  ("Karina Mcguire","imperdiet.ornare.in@protonmail.net","Ap #404-7736 Nibh. Road","3687 344758 77398","449"),
-  ("Cathleen Hughes","nec.urna@icloud.ca","P.O. Box 208, 9218 Sagittis Av.","559623 836666 3165","501"),
-  ("Graiden Mcknight","montes@outlook.org","791-2997 Gravida Av.","490327 2449252926","324");
