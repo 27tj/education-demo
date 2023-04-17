192b42e81b5a7cb91fad5cb84f63b0939fe7cc81
gamethomes38 <gamethomes38@gmail.com>
2023-04-17 15:01:12 +0800

    updated model

--- backend/PythonBackendCode/REST API/Schemas/User_model.py
+++ backend/PythonBackendCode/REST API/Schemas/User_model.py
@@ -2,8 +2,8 @@ from .. import db
 from flask_login import UserMixin
 import sqlalchemy as sa
 
-class Customers(db.Model, UserMixin):
-    uuid = sa.Column(sa.VARBINARY(16), primary_key=True)
+class User(db.Model, UserMixin):
+    uuid = sa.Column(sa.VARBINARY(16), primary_key=True )
     email = sa.Column(sa.VARCHAR(200), unique=True)
     password = sa.Column(sa.VARCHAR(200))
     name = sa.Column(sa.VARCHAR(200))
